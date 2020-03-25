519d360d9148faa764c74c88389de48a3e17245a
newoo <45201399+newoo@users.noreply.github.com>
2020-03-25 21:00:43 +0900

    Update Category.swift
      - Add code to convert dictionary to struct
      - Modify toDictionnary function

--- rfios/data/Category.swift
+++ rfios/data/Category.swift
@@ -14,9 +14,19 @@ struct Category {
     var title = ""
     var color = "#000000"
     
+    init() {
+        
+    }
+    
+    init(_ dic: [String:Any]) {
+        id = dic["category_id"] as? Int ?? -1
+        title = dic["category_title"] as? String ?? ""
+        color = dic["category_color"] as? String ?? "#000000"
+    }
+    
     func toDictionary() -> [String: Any?] {
         var dic: [String: Any?] = [:]
-        dic["id"] = self.id
+        dic["category_id"] = self.id
         dic["category_title"] = self.title
         dic["category_color"] = self.color
         
